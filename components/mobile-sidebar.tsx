c153e95b4210c6d136dded58ad58d03e33e6b437
Ufuk Yıldız <84462888+yldzufukk@users.noreply.github.com>
2025-04-22 16:47:44 +0300

    Sidebar section

---
+++ components/mobile-sidebar.tsx
@@ -0,0 +1,26 @@
+
+
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+  } from "@/components/ui/sheet";
+
+import { Sidebar } from "./sidebar"; 
+import { Menu } from "lucide-react";
+
+export const MobileSidebar = () => {
+    return (
+        <Sheet>
+            <SheetTrigger>
+                <Menu className="text-white"/>
+            </SheetTrigger>
+            <SheetContent className="p-0 z-[100]" side="left">
+                <Sidebar />
+            </SheetContent>
+        </Sheet>
+    )
+}
