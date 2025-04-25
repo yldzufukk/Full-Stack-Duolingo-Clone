f10c2d698dcc5d4809bf9cf3bd3a98cc4be4b474
Ufuk Yıldız <84462888+yldzufukk@users.noreply.github.com>
2025-04-25 16:24:37 +0300

    courses page

---
+++ drizzle.config.ts
@@ -0,0 +1,10 @@
+// drizzle.config.ts
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  dialect: "postgresql",
+  schema: "./src/schema.ts",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+});
