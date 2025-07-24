43f46fa415c8ea28d8855a2e6366d16d37b618ad
Ufuk Yıldız <84462888+yldzufukk@users.noreply.github.com>
2025-07-24 23:27:14 +0300

    bug fixed
    admin page disabled

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,14 +1,14 @@
 import { redirect } from "next/navigation";
-import { isAdmin } from "@/lib/admin";
+//import { isAdmin } from "@/lib/admin";
 import dynamic from "next/dynamic";
 
 // Client tarafında dinamik olarak yüklenen bileşen
 const App = dynamic(() => import("./app"));
 
 export default function AdminPage() {
-  if (!isAdmin()) {
+
     redirect("/");
-  }
+  
 
   return <App />;
 }
--- lib/admin.ts
+++ lib/admin.ts
@@ -1,13 +1,10 @@
-import { auth } from "@clerk/nextjs/server";
-
-const adminIds = ["user_2dGb6YEarBAQHrNYoB5dMtISRWK"];
-
-export const isAdmin = () => {
-  const { userId } = auth();
-
-  if (!userId) {
-    return false;
-  }
-
-  return adminIds.includes(userId);
-};
+//import { auth } from "@clerk/nextjs/server";
+//
+//  const { userId } = auth();
+//
+ // if (!userId) {
+ //   return false;
+//  }
+//
+//  return adminIds.includes(userId);
+//};
